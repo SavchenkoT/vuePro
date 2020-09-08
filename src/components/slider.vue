9a1b97254f558f00e100d221a199f609c19692dd
tatyana <tatyana.savchenko@svitsoft.com>
2020-09-08 11:35:58 +0300

    js changes

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,13 +1,44 @@
 <template>
-    $END$
+    <swiper ref="mySwiper" :options="swiperOptions">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import VueAwesomeSwiper from 'vue-awesome-swiper'
+
     export default {
-        name: "slider"
+        name: 'carrousel',
+        data() {
+            return {
+                swiperOptions: {
+                    pagination: {
+                        el: '.swiper-pagination'
+                    },
+                }
+            }
+        },
+        computed: {
+            swiper() {
+                return this.$refs.mySwiper.$swiper
+            }
+        },
+        mounted() {
+            console.log('Current Swiper instance object', this.swiper)
+            this.swiper.slideTo(3, 1000, false)
+        }
     }
 </script>
 
 <style scoped>
+    import 'vue-h5-swiper'
+    import 'swiper/css/swiper.css'
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.use(VueAwesomeSwiper)
+
 Vue.config.productionTip = false
 
 new Vue({
